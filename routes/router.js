4a1fa4810242f70496b90f53c063e921ca27b37b
Quarksaur <hexostone@gmail.com>
2024-11-28 10:38:17 +0100

    ejs test

--- routes/router.js
+++ routes/router.js
@@ -8,6 +8,16 @@ import mailRouter from "./mail.js";
 
 const router = express.Router();
 
+////////////////////
+// FRONT
+
+router.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+////////////////////
+// BACK
+
 router.use("/admin", adminRouter);
 router.use("/login", loginRouter);
 router.use("/etiquette", etiquetteRouter);
