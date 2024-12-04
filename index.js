fa8b58dbbc9e0720560f56ba641683015eba8f62
Nico-Sng <nicolassung01@gmail.com>
2024-12-04 12:46:17 +0100

    Update Page etiquette
    Modification scss

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/router.js";
+import path from "path";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(router);
 
