a2124d09c09ddd06ac2db8ecf0746ad622d11430
Mathilde Morell <morell.mathilde74@gmail.com>
2024-12-16 14:06:15 +0100

    update server.cjs

--- public/js/emailserver/server.cjs
+++ public/js/emailserver/server.cjs
@@ -1,30 +1,41 @@
-// Importer la bibliothèque Nodemailer
 const nodemailer = require('nodemailer');
+
+
+nodemailer.createTestAccount((err, account) => {
+  if (err) {
+      console.error('Failed to create a testing account. ' + err.message);
+      return process.exit(1);
+  }
+
+  console.log('Credentials obtained, sending message...');
 
 // Créer un objet transporteur
 const transporter = nodemailer.createTransport({
-  host: 'sandbox.smtp.mailtrap.io',
-  port: 2525,
-  secure: false, // utiliser SSL
+  host: 'smtp.ethereal.email',
+  port: 587,
   auth: {
-    user: 'e35f2ed274fdb3',
-    pass: '7f154bb9882c7c',
+      user: 'rhea.mcdermott87@ethereal.email',
+      pass: 'GWvD9bGBnH7BJu78ss'
   }
 });
 
 // Configurer l'objet mailOptions
-const mailOptions = {
-  from: 'info@mailtrap.club',
-  to: 'justinefoghaven@gmail.com',
-  subject: 'Envoyer un email en utilisant Node.js',
-  text: 'Test'
-};
+let message = {
+        from: 'Sender Name <sender@example.com>',
+        to: 'Recipient <recipient@example.com>',
+        subject: 'Nodemailer is unicode friendly ✔',
+        text: 'Hello to myself!',
+        html: '<p><b>Hello</b> to myself!</p>'
+    };
 
-// Envoyer l'email
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log('Erreur:', error);
-  } else {
-    console.log('Email envoyé:', info.response);
-  }
+    transporter.sendMail(message, (err, info) => {
+        if (err) {
+            console.log('Error occurred. ' + err.message);
+            return process.exit(1);
+        }
+
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
 });
